d896597c94db888ece103b84062afe84f185bf3e
ammarmuflih <ammarmuflih95@gmail.com>
2022-12-16 06:34:31 +0700

    update config

--- configs/efficientnet/mask_rcnn_effb3_fpn_1x_coco.py
+++ configs/efficientnet/mask_rcnn_effb3_fpn_1x_coco.py
@@ -1,6 +1,7 @@
 _base_ = [
     '../_base_/models/mask_rcnn_r50_fpn.py',
-    '../_base_/datasets/coco_instance.py', '../_base_/default_runtime.py'
+    '../_base_/datasets/coco_instance.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
 
 cudnn_benchmark = True
@@ -10,7 +11,7 @@ model = dict(
      backbone=dict(
         _delete_=True,
         type='EfficientNet',
-        arch='b3',
+        arch='b6',
         drop_path_rate=0.2,
         out_indices=(3, 4, 5),
         frozen_stages=0,
@@ -21,12 +22,7 @@ model = dict(
             type='Pretrained', prefix='backbone', checkpoint=checkpoint)),
     
     neck=dict(
-        in_channels=[48, 136, 384],
-        start_level=0,
-        out_channels=256,
-        relu_before_extra_convs=True,
-        no_norm_on_lateral=True,
-        norm_cfg=norm_cfg),
+        in_channels=[48, 136, 384]),
 
     #bbox_head=dict(type='RetinaSepBNHead', num_ins=5, norm_cfg=norm_cfg),
     # training and testing settings
@@ -36,68 +32,68 @@ model = dict(
         mask_head=dict(
             num_classes=2))
 )
-# dataset settings
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-img_size = (896, 896)
-train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
-    dict(
-        type='Resize',
-        img_scale=img_size,
-        ratio_range=(0.8, 1.2),
-        keep_ratio=True),
-    dict(type='RandomCrop', crop_size=img_size),
-    dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size=img_size),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels','gt_masks']),
-]
-test_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(
-        type='MultiScaleFlipAug',
-        img_scale=img_size,
-        flip=False,
-        transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='RandomFlip'),
-            dict(type='Normalize', **img_norm_cfg),
-            dict(type='Pad', size=img_size),
-            dict(type='ImageToTensor', keys=['img']),
-            dict(type='Collect', keys=['img']),
-        ])
-]
+# # dataset settings
+# img_norm_cfg = dict(
+#     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+# img_size = (896, 896)
+# train_pipeline = [
+#     dict(type='LoadImageFromFile'),
+#     dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+#     dict(
+#         type='Resize',
+#         img_scale=img_size,
+#         ratio_range=(0.8, 1.2),
+#         keep_ratio=True),
+#     dict(type='RandomCrop', crop_size=img_size),
+#     dict(type='RandomFlip', flip_ratio=0.5),
+#     dict(type='Normalize', **img_norm_cfg),
+#     dict(type='Pad', size=img_size),
+#     dict(type='DefaultFormatBundle'),
+#     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels','gt_masks']),
+# ]
+# test_pipeline = [
+#     dict(type='LoadImageFromFile'),
+#     dict(
+#         type='MultiScaleFlipAug',
+#         img_scale=img_size,
+#         flip=False,
+#         transforms=[
+#             dict(type='Resize', keep_ratio=True),
+#             dict(type='RandomFlip'),
+#             dict(type='Normalize', **img_norm_cfg),
+#             dict(type='Pad', size=img_size),
+#             dict(type='ImageToTensor', keys=['img']),
+#             dict(type='Collect', keys=['img']),
+#         ])
+# ]
 
 dataset_type = 'COCODataset'
 classes = ('person','car')
 data = dict(
     samples_per_gpu=1,
-    workers_per_gpu=1,
-    train=dict(pipeline=train_pipeline),
-    val=dict(pipeline=test_pipeline),
-    test=dict(pipeline=test_pipeline)
+    workers_per_gpu=1
+    # train=dict(pipeline=train_pipeline),
+    # val=dict(pipeline=test_pipeline),
+    # test=dict(pipeline=test_pipeline)
 )
 
-# optimizer
-optimizer_config = dict(grad_clip=None)
-optimizer = dict(
-    type='SGD',
-    lr=0.001,
-    momentum=0.9,
-    weight_decay=0.0001)
+# # optimizer
+# optimizer_config = dict(grad_clip=None)
+# optimizer = dict(
+#     type='SGD',
+#     lr=0.001,
+#     momentum=0.9,
+#     weight_decay=0.0001)
 
-# learning policy
-lr_config = dict(
-    policy='step',
-    warmup='linear',
-    warmup_iters=500,
-    warmup_ratio=0.001,
-    step=[8, 11])
+# # learning policy
+# lr_config = dict(
+#     policy='step',
+#     warmup='linear',
+#     warmup_iters=500,
+#     warmup_ratio=0.001,
+#     step=[8, 11])
 # runtime settings
-runner = dict(type='EpochBasedRunner', max_epochs=100)
+runner = dict(type='EpochBasedRunner', max_epochs=10)
 
 # NOTE: `auto_scale_lr` is for automatically scaling LR,
 # USER SHOULD NOT CHANGE ITS VALUES.
--- configs/mask_rcnn/mask_rcnn_r101_fpn_1x_coco.py
+++ configs/mask_rcnn/mask_rcnn_r101_fpn_1x_coco.py
@@ -2,4 +2,32 @@ _base_ = './mask_rcnn_r50_fpn_1x_coco.py'
 model = dict(
     backbone=dict(
         depth=101,
-        init_cfg=None))
+        init_cfg=None),
+        
+roi_head=dict(
+        bbox_head=dict(
+            num_classes=2),
+        mask_head=dict(
+            num_classes=2))
+)
+
+
+dataset_type = 'COCODataset'
+classes = ('person','car')
+data = dict(
+    samples_per_gpu=1,
+    workers_per_gpu=1
+    # train=dict(pipeline=train_pipeline),
+    # val=dict(pipeline=test_pipeline),
+    # test=dict(pipeline=test_pipeline)
+)
+
+runner = dict(type='EpochBasedRunner', max_epochs=100)
+
+# NOTE: `auto_scale_lr` is for automatically scaling LR,
+# USER SHOULD NOT CHANGE ITS VALUES.
+# base_batch_size = (8 GPUs) x (4 samples per GPU)
+auto_scale_lr = dict(base_batch_size=1)
+
+evaluation = dict(interval=10)
+
