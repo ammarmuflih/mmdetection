38404ae9e0e4dde939746421cec316480c79da85
ammarmuflih <ammarmuflih95@gmail.com>
2022-12-19 16:25:16 +0700

    update config

--- configs/efficientnet/mask_rcnn_effb3_fpn_1x_coco_2.py
+++ configs/efficientnet/mask_rcnn_effb3_fpn_1x_coco_2.py
@@ -73,8 +73,8 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=1,
-    workers_per_gpu=1,
+    samples_per_gpu=4,
+    workers_per_gpu=4,
     train=dict(pipeline=train_pipeline),
     val=dict(pipeline=test_pipeline),
     test=dict(pipeline=test_pipeline))
