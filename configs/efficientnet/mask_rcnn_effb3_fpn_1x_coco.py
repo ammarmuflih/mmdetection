b1cdbe46cae9009ae807e774ce040997c1f71bdc
ammarmuflih <ammarmuflih95@gmail.com>
2022-12-19 14:02:31 +0700

    update config

--- configs/efficientnet/mask_rcnn_effb3_fpn_1x_coco.py
+++ configs/efficientnet/mask_rcnn_effb3_fpn_1x_coco.py
@@ -8,7 +8,7 @@ _base_ = [
 dataset_type = 'COCODataset'
 classes = ('person','car')
 data = dict(
-    samples_per_gpu=1,
+    samples_per_gpu=4,
     workers_per_gpu=1,
     train=dict(
         img_prefix='/home/ammar/Documents/mmdetection/data/coco/train2017/',
@@ -24,7 +24,7 @@ data = dict(
         ann_file='/home/ammar/Documents/mmdetection/data/coco/annotations/filtered_labels_test.json'))
 
 
-checkpoint = '/home/ammar/Documents/mmdetection/efficientnet-b5-b6417697.pth'
+checkpoint = '/home/ammar/Documents/mmdetection/work_dirs/epoch_100.pth'
 cudnn_benchmark = True
 norm_cfg = dict(type='BN', requires_grad=True)
 model = dict(
@@ -34,7 +34,7 @@ model = dict(
         arch='b5',
         drop_path_rate=0.2,
         out_indices=(3, 4, 5),
-        frozen_stages=0,
+        frozen_stages=5,
         norm_cfg=dict(
             type='BN', requires_grad=True, eps=1e-3, momentum=0.01),
         norm_eval=False,
@@ -42,13 +42,7 @@ model = dict(
             type='Pretrained', prefix='backbone', checkpoint=checkpoint)),
 
     neck=dict(
-        type='FPN',
-        in_channels=[40, 112, 320],
-        start_level=0,
-        out_channels=256,
-        relu_before_extra_convs=True,
-        no_norm_on_lateral=True,
-        norm_cfg=norm_cfg),
+        in_channels=[64, 176, 512]),
     
     train_cfg=dict(assigner=dict(neg_iou_thr=0.5)),
 
@@ -68,14 +62,15 @@ optimizer = dict(
     type='SGD',
     lr=0.001,
     momentum=0.9,
-    weight_decay=0.001,
-    paramwise_cfg=dict(norm_decay_mult=0, bypass_duplicate=True))
+    weight_decay=0.0001,
+ #   paramwise_cfg=dict(norm_decay_mult=0, bypass_duplicate=True)
+ )
 # learning policy
 lr_config = dict(
     policy='step',
     warmup='linear',
     warmup_iters=1000,
-    warmup_ratio=0.1,
+    warmup_ratio=0.001,
     step=[8, 11])
 # runtime settings
 runner = dict(type='EpochBasedRunner', max_epochs=30)
@@ -83,7 +78,7 @@ runner = dict(type='EpochBasedRunner', max_epochs=30)
 # NOTE: `auto_scale_lr` is for automatically scaling LR,
 # USER SHOULD NOT CHANGE ITS VALUES.
 # base_batch_size = (8 GPUs) x (4 samples per GPU)
-auto_scale_lr = dict(base_batch_size=1)
+auto_scale_lr = dict(base_batch_size=16)
 
 #Eval interval
 evaluation = dict(interval=10)
